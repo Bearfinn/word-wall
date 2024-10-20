6520a25c3e87bbd56f127d7db2b14efebf2bc553
Kritsada <kritsada.stw@gmail.com>
2024-10-20 23:26:19 +0700

    feat: use vue router dynamic url

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,12 +3,21 @@ import WordWall from '../components/WordWall.vue'
 import { WORD_GROUPS } from '../data/list'
 
 type Writable<T> = { -readonly [P in keyof T]: T[P] };
-const routes: Writable<RouterOptions['routes']> = Object.keys(WORD_GROUPS).map(key => ({
-  path: `/board/${key}`,
-  name: key,
-  component: WordWall,
-  props: { wordGroup: WORD_GROUPS[key] }
-}))
+
+const routes: Writable<RouterOptions['routes']> = [
+  {
+    path: '/board/:boardId',
+    name: 'Board',
+    component: WordWall,
+    props: (route) => ({ 
+      wordGroup: WORD_GROUPS[route.params.boardId as string] || []
+    })
+  },
+  {
+    path: '/',
+    redirect: `/board/${Object.keys(WORD_GROUPS)[0]}`
+  }
+];
 
 routes.push({ path: '/', redirect: `/board/${Object.keys(WORD_GROUPS)[0]}` })
 
