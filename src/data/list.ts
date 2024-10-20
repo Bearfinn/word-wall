e4310ef033f70492a24a92c7c256bbbc956ba65f
Kritsada <kritsada.stw@gmail.com>
2024-10-20 23:18:03 +0700

    feat(board): add kpop 1 board

--- src/data/list.ts
+++ src/data/list.ts
@@ -13,4 +13,10 @@ export const WORD_GROUPS: Record<string, WordGroup[]> = {
     { groupName: "Meats", words: ["beef", "chicken", "pork", "lamb"] },
     { groupName: "Grains", words: ["rice", "wheat", "corn", "barley"] },
   ],
+  "kpop-board-1": [
+    { groupName: "Has a song named 'Fire'", words: ["BTS", "EXID", "SEVENTEEN", "TWICE"] },
+    { groupName: "Has 12 members at debut", words: ["LOONA", "IZ*ONE", "WJSN", "Treasure"] },
+    { groupName: "Has one member left the group", words: ["(G)I-DLE", "LE SSERAFIM", "NMIXX", "Stray Kids"] }, 
+    { groupName: "Formed as a project group and disbanded", words: ["I.O.I", "Wanna One", "X1", "Mamadol"] },
+  ]
 };
